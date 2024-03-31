991c40c82ba6cf63ac39a31ed1cfba821c7982e2
mirzayev otbek <otabek.@gmail.com>
2024-03-31 17:53:39 +0300

    Vite + React

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All Right Reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
